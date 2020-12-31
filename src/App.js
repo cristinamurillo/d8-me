a64babd6c905267eaddb8eab5db5523f4bb5281c
Cristina Murillo <murillocam96@gmail.com>
2020-12-31 00:08:36 -0600

    animated clouds

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import cloud from './assets/clouds.svg';
 
 function App() {
   return (
     <div className="App">
+      <img src={cloud} alt="cloud" className="cloud top"/>
+      <img src={cloud} alt="cloud" className="cloud middle"/>
+      <img src={cloud} alt="cloud" className="cloud bottom"/>
       <h1 className="header">Let's go on a date!</h1>
     </div>
   );
