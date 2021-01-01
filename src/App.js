40bfed8aaf9460565699a83fced7cc5be1edeb6a
cristinamurillo <41346622+cristinamurillo@users.noreply.github.com>
2020-12-31 23:11:40 -0600

    Clouds (#2)
    * animated clouds
    
    * styling, balloon placeholder
    
    * cloud outline, styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import cloud from './assets/clouds.svg';
+import balloon from './assets/hot-air-balloon.png';
 
 function App() {
   return (
     <div className="App">
+      <img src={cloud} alt="cloud" className="cloud top"/>
+      <img src={cloud} alt="cloud" className="cloud middle"/>
+      <img src={cloud} alt="cloud" className="cloud bottom"/>
       <h1 className="header">Let's go on a date!</h1>
+      <img src={balloon} alt="hot air balloon" className="balloon"/>
     </div>
   );
 }
