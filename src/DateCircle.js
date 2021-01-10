a4c083a98422c489e9c53ef3144574e8dead173f
Cristina Murillo <murillocam96@gmail.com>
2021-01-09 23:48:50 -0600

    positioning... my nightmare

--- src/DateCircle.js
+++ src/DateCircle.js
@@ -3,11 +3,16 @@ import {BG_IMAGE_WIDTH_PX} from './App';
 
 export default function DateCircle() {
   const radius = 20;
-  const circumference = Math.PI * (radius * 2);
   const circleIds = ['circle-one', 'circle-two', 'circle-three']
+  const container = { 
+    transform: `translate(${-1 * (window.innerWidth - BG_IMAGE_WIDTH_PX)}px, 0px)` 
+};
   
   return (
-    <div className="date-circle-container">
+    <div className="date-circle-container" style={container}>
+      <img
+        alt="date"
+        src="https://images.unsplash.com/photo-1550865811-5db66d4e19ac?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80"/>
       <svg
         className="spinner"
         width="700"
@@ -17,6 +22,7 @@ export default function DateCircle() {
         {circleIds.map((id) => {
           return(
             <circle
+              key={id}
               id={id}
               className="drawn-circle"
               cx="350"
@@ -24,7 +30,7 @@ export default function DateCircle() {
               r={`${radius}vh`}
               fill="none"
               strokeWidth="20"
-              strokeDashoffset={`${circumference/2}vh`}
+              strokeDashoffset="0"
               strokeLinecap="round"
             />
           )
