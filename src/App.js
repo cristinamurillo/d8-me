b306203c766ba1afec8b65a43b2a0cddfeddaf9c
cristinamurillo <41346622+cristinamurillo@users.noreply.github.com>
2021-01-12 14:59:31 -0600

    Fun circle (#3)
    * title + title icon
    
    * initial scroll and circle
    
    * more responsive translation
    
    * good enough...
    
    * positioning... my nightmare
    
    * lil adjustments
    
    * it is what it is
    
    * improved balloon animation
    
    * package lock
    
    * description positioning

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
+import {useRef} from 'react';
+import styled, {keyframes} from 'styled-components';
 import './App.css';
 import cloud from './assets/clouds.svg';
 import balloon from './assets/hot-air-balloon.png';
 import smileyCloud from './assets/smiley-cloud.svg';
+import DateCircle from './DateCircle';
+
+const BG_IMAGE_WIDTH_PX = 3000
+const bgTranslationPixels = window.innerWidth - BG_IMAGE_WIDTH_PX
+const moveBalloon = keyframes`
+  0% {
+    transform: translateX(0px) rotate(0deg)
+  }
+  50% {
+    transform: translateX(${(-1 * bgTranslationPixels) * 2 / 3}px) rotate(20deg)
+  }
+  100% {
+    transform: translateX(${-1 * bgTranslationPixels}px) rotate(0deg)
+  }
+`;
+
+const HotAirBalloon = styled.img`
+  height: 25rem;
+  position: absolute;
+  left: 15vw;
+  top: 30vh;
+  animation: ${moveBalloon} 3s paused;
+  transform: translateX(${-1 * bgTranslationPixels}px) rotate(0deg);
+`;
 
 function App() {
+  const backgroundRef = useRef();
+  const balloonRef = useRef()
+
+  const scrollToDateCircle = () => {
+    backgroundRef.current.style.transform = `translate(${bgTranslationPixels}px, 0)`;
+    balloonRef.current.style.animationPlayState = 'running';
+  };
+
   return (
-    <div className="App">
+    <div className={`App`} ref={backgroundRef}>
       <img src={cloud} alt="cloud" className="cloud top"/>
       <img src={cloud} alt="cloud" className="cloud middle"/>
       <img src={cloud} alt="cloud" className="cloud bottom"/>
         <span className="center-header">
           <h1 className="header">Let's go on a date!</h1>
-          <img src={smileyCloud} alt="smiling cloud" id="smiling-cloud"/>
+          <img
+            onClick={scrollToDateCircle}
+            src={smileyCloud}
+            alt="smiling cloud"
+            id="smiling-cloud"/>
         </span>
-      <img src={balloon} alt="hot air balloon" className="balloon"/>
+      <HotAirBalloon ref={balloonRef} src={balloon} alt="hot air balloon" className="balloon"/>
+      <DateCircle />
     </div>
   );
 }
 
 export default App;
+export {BG_IMAGE_WIDTH_PX}
---
+++ src/DateCircle.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {BG_IMAGE_WIDTH_PX} from './App';
+
+export default function DateCircle() {
+  const radius = 25;
+  const circleIds = ['circle-one', 'circle-two', 'circle-three']
+  const container = { 
+    transform: `translate(${-1 * (window.innerWidth - BG_IMAGE_WIDTH_PX)}px, 0px)` 
+};
+  
+  return (
+    <div className="date-circle-container" style={container}>
+      <h2 className="lg-description">This is the date description. Fun fun :-).</h2>
+      <img
+        alt="date"
+        src="https://images.unsplash.com/photo-1550865811-5db66d4e19ac?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80"/>
+      <svg
+        className="spinner"
+        width="700"
+        height="700"
+        viewBox="0 0 700 700"
+      >
+        {circleIds.map((id) => {
+          return(
+            <circle
+              key={id}
+              id={id}
+              cx="350"
+              cy="350"
+              r={`${radius}vh`}
+              fill="none"
+              strokeWidth="20"
+              strokeDashoffset="0"
+              strokeLinecap="round"
+            />
+          )
+        })}
+      </svg>
+    </div>
+  );
+
+}
